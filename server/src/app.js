c33180a1b63adb9134f38dec83af270a062fe8eb
sudhanshu khosla <sudhanshukhosla123@gmail.com>
2025-02-03 19:17:03 +0530

    check server

--- server/src/app.js
+++ server/src/app.js
@@ -6,16 +6,23 @@ import morgan from 'morgan';
 const app = express()
 
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+app.use(cors(
+    {
+        origin: ['https://nextube-eta.vercel.app'],
+        methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+        credentials: true,
+    }
+))
+
 app.use(morgan('dev'));
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+app.get("/", (req, res) => {
+    res.json("hello");
+})
 
 //routes import
 import userRouter from './routes/user.routes.js'
